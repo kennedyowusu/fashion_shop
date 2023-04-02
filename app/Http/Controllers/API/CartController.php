885c080d2d63966effdda2efb70f273f5da2d5cb
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-02 14:12:57 +0000

    create cart controller, cart resource and cart request files

---
+++ app/Http/Controllers/API/CartController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CartRequest;
+use App\Http\Resources\CartResource;
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $carts = Cart::all();
+        return response()->json($carts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CartRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $validatedData = $request->validated();
+            $user = Auth::user();
+            $product = Product::findOrFail($validatedData['product_id']);
+            $cart = new Cart($validatedData);
+            $cart->user_id = $user->id;
+            $cart->price = $product->price;
+            $cart->save();
+            $this->saveImage($request->image, 'carts', 300, 300);
+            DB::commit();
+            return new CartResource($cart);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($request->validated());
+
+            DB::rollBack();
+            return response()->json(['error' => 'Failed to create cart.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cart $cart)
+    {
+        try {
+            return new CartResource($cart);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cart Item not found.'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CartRequest $request, Cart $cart)
+    {
+        try {
+            DB::beginTransaction();
+            $cart->update($request->validated());
+            $this->saveImage($request->image, 'carts', 300, 300);
+            DB::commit();
+            return new CartResource($cart);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($request->all());
+
+            DB::rollBack();
+            return response()->json(['error' => 'Failed to update cart.'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cart $cart)
+    {
+        DB::beginTransaction();
+
+        try {
+            $cart->delete();
+            DB::commit();
+            return response()->json(['message' => 'Cart Item deleted successfully.'], 200);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+
+            DB::rollBack();
+            return response()->json(['error' => 'Failed to delete cart.'], 500);
+        }
+    }
+
+}
---
+++ app/Http/Requests/CartRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'quantity' => 'required|integer|min:0',
+            'image' => 'sometimes|image|max:2048|mimes:jpeg,png,gif',
+            'user_id' => 'required|integer|exists:users,id',
+            'product_id' => 'required|integer|exists:products,id',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is required',
+            'name.string' => 'Name must be a string',
+            'name.max' => 'Name must be less than 255 characters',
+            'price.required' => 'Price is required',
+            'price.numeric' => 'Price must be a number',
+            'price.min' => 'Price must be greater than 0',
+            'quantity.required' => 'Quantity is required',
+            'quantity.integer' => 'Quantity must be an integer',
+            'quantity.min' => 'Quantity must be greater than 0',
+            'image.required' => 'Image is required',
+            'image.image' => 'Image must be an image',
+            'image.max' => 'Image must be less than 2MB',
+            'image.mimes' => 'Image must be a file of type: jpeg, png, gif',
+            'user_id.required' => 'User is required',
+            'user_id.integer' => 'User must be an integer',
+            'user_id.exists' => 'User does not exist',
+            'product_id.required' => 'Product is required',
+            'product_id.integer' => 'Product must be an integer',
+            'product_id.exists' => 'Product does not exist',
+        ];
+    }
+}

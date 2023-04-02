df2c797ee828125c1eedc7f12cd9d4278b76772c
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-02 14:47:28 +0000

    set the user_id and product_id foreign keys explicitly in the cart seeder to ensure that they are not null

--- database/seeders/CartSeeder.php
+++ database/seeders/CartSeeder.php
@@ -6,13 +6,25 @@ use App\Models\Cart;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use App\Models\Product;
+
 class CartSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Cart::factory(5)->create();
+        // get some users and products
+        $users = User::all();
+        $products = Product::all();
+
+        // create 5 carts with random users and products
+        Cart::factory(5)
+            ->make()
+            ->each(function ($cart) use ($users, $products) {
+                $cart->user_id = $users->random()->id;
+                $cart->product_id = $products->random()->id;
+                $cart->save();
+            });
     }
 }
+
